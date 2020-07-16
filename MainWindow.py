c68935d767488d00ad0df2e75efe1e6945bc3607
Tamas Bodnar <t.bodnar@auersignal.com>
2020-07-16 15:10:29 +0200

    zoom is almost done

--- MainWindow.py
+++ MainWindow.py
@@ -8,10 +8,11 @@ from pathlib import Path
 from baby import Baby
 import os
 from pattern_canvas.PatternCanvas import PatternCanvas
+from simulator import Simulator
 
 pattern_wrapper_options = {
     'width': '220',
-    'height': '725',
+    'height': '700',
     'bg': 'White',
     'bd': '5'
 }
@@ -24,13 +25,13 @@ pattern_canvas_options = {
 habitat_canvas_options = {
     'width': '800',
     'bg': 'White',
-    'height': '753',
+    'height': '725',
     'bd': '5'
 }
 patterns = []
 
 root = Tk()
-root.geometry('1024x768')
+root.geometry('1024x800')
 root.title('Game of life')
 root.resizable(0, 0)
 
@@ -60,22 +61,20 @@ def open_folder_dialog():
             pattern.close()
 
 
-def start_simulation():
-    print('sim started')
-
-
 def pattern_clicked(event):
-    for widget in habitat_canvas.winfo_children():
-        widget.destroy()
+    sim_canvas.delete(ALL)
 
-    print(len(event.widget.baby.cells), event.widget.baby.name)
-    print(event.widget.find_closest(event.x, event.y))
-    sim_canvas = PatternCanvas(habitat_canvas, habitat_canvas_options)
-    sim_canvas.grid(column=0, row=0, sticky=N + W + S + E)
-    create_and_draw_sim_canvas(event.widget.baby, sim_canvas)
+    create_and_draw_sim_canvas(event.widget.baby)
 
 
-def create_and_draw_sim_canvas(baby, sim_canvas):
+def start_simulation():
+    if len(sim_canvas.baby.cells) > 0:
+        Simulator.start_simulation(sim_canvas, speed_scale, size_scale)
+    else:
+        messagebox.showinfo("Choose a pattern!", "Please, click on a pattern")
+
+
+def create_and_draw_sim_canvas(baby):
     sim_canvas.set_baby(baby)
     sim_canvas.fill_canvas_to_live()
 
@@ -93,7 +92,7 @@ def load_pattern(file, row):
 
 # create patterns canvas to show patterns visually
 def create_and_fill_pattern_canvas(baby, row):
-    for widget in habitat_canvas.winfo_children():
+    for widget in sim_canvas.winfo_children():
         widget.destroy()
     pattern_canvas = PatternCanvas(pattern_wrapper, pattern_canvas_options)
     pattern_canvas.grid(column=0, row=row, sticky=N + W)
@@ -101,6 +100,14 @@ def create_and_fill_pattern_canvas(baby, row):
     pattern_canvas.fill_canvas_with_baby_cells()
     pattern_wrapper.create_window(100, 108 * (1 + row) + row * 108, window=pattern_canvas)
     pattern_canvas.bind("<Button-1>", pattern_clicked)
+
+
+def update_size_scale(event):
+    Simulator.change_size(event, sim_canvas)
+
+
+def update_speed_scale(event):
+    Simulator.change_speed(event)
 
 
 # menu
@@ -114,18 +121,30 @@ app_menu.add_command(label="Exit", compound='left', command=exit_callback)
 root.config(menu=menu_bar)
 
 pattern_wrapper = Canvas(root, pattern_wrapper_options, scrollregion=(0, 0, 1000, 1000))
-pattern_wrapper.grid(column=0, row=0, rowspan=2, sticky=W + S + N + E, )
+pattern_wrapper.grid(column=0, row=0, rowspan=1, sticky=W + S + N + E, padx=(2, 0), pady=(2, 2))
 vbar = Scrollbar(root, orient=VERTICAL)
-vbar.grid(column=1, row=0, rowspan=2, sticky=W + S + N)
+vbar.grid(column=1, row=0, rowspan=1, sticky=W + S + N)
 vbar.config(command=pattern_wrapper.yview)
 
 pattern_wrapper.configure(yscrollcommand=vbar.set)
 pattern_wrapper.config(scrollregion=pattern_wrapper.bbox(ALL))
 
 start_button = Button(root, text='Start simulation', command=start_simulation)
-start_button.grid(column=0, row=1, sticky=S + E + W)
+start_button.grid(column=0, row=1, sticky=E + W, padx=2, pady=2)
 
-habitat_canvas = Canvas(root, habitat_canvas_options)
-habitat_canvas.grid(column=2, row=0, rowspan=2, sticky=E + N)
+sim_canvas = PatternCanvas(root, habitat_canvas_options)
+sim_canvas.grid(column=2, row=0, sticky=E + N, columnspan=2, padx=2, pady=2)
+
+size_scale = Scale(root, from_=0, to=100, length=250, orient=HORIZONTAL, label='Zoom', showvalue=0,
+                   command=update_size_scale, resolution=5)
+size_scale.set(1)
+size_scale.grid(column=2, row=1, sticky=W + S)
+size_scale.grid_columnconfigure(3, weight=2)
+
+speed_scale = Scale(root, from_=100, to=1, length=250, orient=HORIZONTAL, label='Speed', showvalue=0,
+                    command=update_speed_scale)
+speed_scale.set(1)
+speed_scale.grid(column=3, row=1, sticky=W + S, )
+speed_scale.grid_columnconfigure(5, weight=2)
 
 root.mainloop()
--- simulator/Simulator.py
+++ simulator/Simulator.py
@@ -1,3 +1,20 @@
+pre_size = 0
 
-def start_simulation(file):
-    print('start ' + file.name)
+
+def start_simulation(habitat_canvas):
+    print(len(habitat_canvas.baby.cells), habitat_canvas.baby.name)
+
+
+def change_size(size_value, hab_canvas):
+    print(size_value)
+    global pre_size
+    if pre_size < float(size_value) or pre_size == 0:
+        size_mod = 0.9
+    else:
+        size_mod = 1.111111
+    hab_canvas.scale('all', 0, 0, size_mod, size_mod)
+    pre_size = float(size_value)
+
+
+def change_speed(speed_value):
+    print(speed_value)
